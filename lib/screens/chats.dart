ebf8429ac1b3d03d2d1f230c2ccb2547a720d09f
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-01-28 22:36:43 -0300

    style: add user image to header and modal

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -5,7 +5,188 @@ import 'package:whatsapp/components/conversation.dart';
 import 'package:whatsapp/components/status_bar.dart';
 
 class ChatsScreen extends StatelessWidget {
-  Widget header(Size size, ThemeData theme) {
+  void showModal(BuildContext context) {
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (builder) {
+        return Container(
+          padding: const EdgeInsets.only(top: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10.0),
+              topRight: Radius.circular(10.0),
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(
+                  10.0,
+                ),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100.0),
+                        image: DecorationImage(
+                          image: AssetImage("assets/user.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Your name here!",
+                            style: GoogleFonts.istokWeb(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            "444-555-1994",
+                            style: GoogleFonts.istokWeb(),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(
+                  5.0,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[300],
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.userFriends,
+                    size: 20.0,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    "Create New Group",
+                    style: GoogleFonts.istokWeb(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[300],
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.commentDots,
+                    size: 23.0,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    "New Transmission",
+                    style: GoogleFonts.istokWeb(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[300],
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.desktop,
+                    size: 20.0,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    "Whatsapp Web",
+                    style: GoogleFonts.istokWeb(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[300],
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.heart,
+                    size: 20.0,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    "Favorites Messages",
+                    style: GoogleFonts.istokWeb(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[300],
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.cog,
+                    size: 20.0,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    "Settings",
+                    style: GoogleFonts.istokWeb(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget header(Size size, ThemeData theme, BuildContext context) {
     return Container(
       height: 50.0,
       padding: const EdgeInsets.all(10.0),
@@ -42,28 +223,40 @@ class ChatsScreen extends StatelessWidget {
               ],
             ),
           ),
-          Container(
-            width: size.width * 0.40 - 10.0,
-            height: 30.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Icon(FontAwesomeIcons.camera, size: 20.0,),
-                Icon(FontAwesomeIcons.search, size: 20.0,),
-                Container(
-                  width: 25.0,
-                  height: 25.0,
-                  decoration: BoxDecoration(
-                    color: Colors.redAccent,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(
-                        100.0,
+          GestureDetector(
+            onTap: () => this.showModal(context),
+            child: Container(
+              width: size.width * 0.40 - 10.0,
+              height: 30.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.camera,
+                    size: 20.0,
+                  ),
+                  Icon(
+                    FontAwesomeIcons.search,
+                    size: 20.0,
+                  ),
+                  Container(
+                    width: 26.0,
+                    height: 26.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          100.0,
+                        ),
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage("assets/user.jpg"),
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
@@ -81,7 +274,7 @@ class ChatsScreen extends StatelessWidget {
           children: <Widget>[
             Column(
               children: <Widget>[
-                this.header(size, theme),
+                this.header(size, theme, context),
                 StatusBar(),
                 Expanded(
                   child: Conversation(),
@@ -153,7 +346,12 @@ class ChatsScreen extends StatelessWidget {
                   Container(
                     padding: const EdgeInsets.all(20.0),
                     decoration: BoxDecoration(
-                      color: Color.fromRGBO(247, 255, 0, 1.0,),
+                      color: Color.fromRGBO(
+                        247,
+                        255,
+                        0,
+                        1.0,
+                      ),
                       borderRadius: BorderRadius.circular(100.0),
                       boxShadow: [
                         BoxShadow(
